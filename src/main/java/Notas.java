b63ec188fe747775991f9b83a6e1f388a06fb232
DannaHA04 <huamanalvarezdanna@gmail.com>
2025-04-16 16:20:37 -0500

    Agregando nuevos codigos

--- src/main/java/Notas.java
+++ src/main/java/Notas.java
@@ -1,12 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Danna
- */
 public class Notas {
+    
+    //Atributos Encapsulados
+    private double Nota1, Nota2, Nota3, Promedio;
+    
+    //Constructor 
+
+    public Notas(double Nota1, double Nota2, double Nota3, double Promedio) {
+        this.Nota1 = Nota1;
+        this.Nota2 = Nota2;
+        this.Nota3 = Nota3;
+        this.Promedio = Promedio;
+    }
+    
+    //Metodos Get y Set
+
+    public double getNota1() {
+        return Nota1;
+    }
+
+    public void setNota1(double Nota1) {
+        this.Nota1 = Nota1;
+    }
+
+    public double getNota2() {
+        return Nota2;
+    }
+
+    public void setNota2(double Nota2) {
+        this.Nota2 = Nota2;
+    }
+
+    public double getNota3() {
+        return Nota3;
+    }
+
+    public void setNota3(double Nota3) {
+        this.Nota3 = Nota3;
+    }
+
+    public double getPromedio() {
+        return Promedio;
+    }
+
+    public void setPromedio(double Promedio) {
+        this.Promedio = Promedio;
+    }
+    
     
 }
